89e55f6326f125d12445af4c0b115eeb234edf41
Samuel J Voeller <samuel.voeller@amethyst.live>
2020-05-25 20:46:46 -0500

    chore(cleanup): removed files that served no purpose

--- test.js
+++
@@ -1,8 +0,0 @@
-async function runtime () {
-  const { fetch } = require('./index')
-  const driver = await fetch('sha256')
-
-  const hashed = await driver.digestIter('AAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'hex', '10000')
-  console.info('logged-hash', hashed)
-}
-runtime()
