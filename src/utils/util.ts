48f36a40e30148a1fc10665f6e9981edb8d692cb
Samuel Voeller <sammyvoeller@gmail.com>
2020-09-25 16:46:32 -0500

    fix: secure generate() cryptographically and expand character set to ASCII 1 byte character list

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,9 +1,13 @@
+import { randomBytes } from 'crypto'
+
 export function generate (bytes: number): string {
-  const char = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890!*_-+=@#$%^&?[]{}'.split('')
+  // const char = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890!";#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split('')
+  const char = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
 
   let generated = ''
   while (generated.length < bytes) {
-    const gChar = char[Math.floor(Math.random() * char.length)]
+    const prng = randomBytes(1)[0] / 255
+    const gChar = char[Math.floor(prng * char.length)]
     generated = generated + gChar
   }
   return generated
