ae81ea06b65b9793499976e443813fd59cd3a849
Samuel Voeller <samuel.voeller@amethyst.live>
2020-09-24 20:01:58 -0500

    tests(cipher, hashing): remove deprecated fetch in favor of getCipher and getHasher respectively

--- tests/hashing.test.ts
+++ tests/hashing.test.ts
@@ -1,13 +1,12 @@
 import { getHashes } from 'crypto';
-import { fetch } from '../src/driver';
-import { HashingDriver } from '../src/driver.hashing';
+import { getHasher } from '../src/driver';
 import { generate } from '../src/utils/util';
 
 describe('Crypto#getHashes: Testing integrity of all provided algorithms.', function () {
   for (const identifier of getHashes()) {
     it (`${identifier}: should not fail single hashing cycle (1)`, async function () {
       for (let index = 0; index < 5; index++) {
-        const driver = <HashingDriver> fetch(identifier)
+        const driver = getHasher(identifier)
         await driver.digest({
           content: generate(1024),
           digest: 'hex'
@@ -16,7 +15,7 @@ describe('Crypto#getHashes: Testing integrity of all provided algorithms.', func
     })
     it (`${identifier}: should not fail multiple hashing cycle (2048)`, async function () {
       for (let index = 0; index < 5; index++) {
-        const driver = <HashingDriver> fetch(identifier)
+        const driver = getHasher(identifier)
         await driver.digest({
           content: generate(1024),
           digest: 'hex',
