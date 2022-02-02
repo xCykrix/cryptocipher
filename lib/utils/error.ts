58cf550ca7c013caa3c811df4ba4eb61edc17ab3
Samuel J Voeller <12182390+xCykrix@users.noreply.github.com>
2022-02-02 23:31:29 +0100

    feat: normalize error message

--- lib/utils/error.ts
+++ lib/utils/error.ts
@@ -1,6 +1,6 @@
 
 export function disabledIdentifier(identifier: string): Error {
-  return new Error(`sec:violation:id_disabled: This identifier [${identifier}] has been disabled due to security or instability concerns.`);
+  return new Error(`sec:violation:id_disabled: This identifier [${identifier}] has been disabled due to security or stability concerns.`);
 }
 
 export function unknownIdentifier(identifier: string): Error {
